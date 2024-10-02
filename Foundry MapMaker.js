78a24378d0086a14d25d5b92920bf104728ae809
Maxoflions <maxoflions@gmail.com>
2024-10-02 11:07:19 -0700

    reorganize into functions

--- Foundry MapMaker.js
+++ Foundry MapMaker.js
@@ -73,7 +73,7 @@ function colorSimilarity(color1, color2) {
 }
 
 function terrainFromColor(color) {
-  return terrainNames.toSorted((a, b) => colorSimilarity(color, terrains[b].color) - colorSimilarity(color, terrains[a].color))[0]
+  return terrainNames.toSorted((a, b) => colorSimilarity(color, terrains[b].color) - colorSimilarity(color, terrains[a].color))[0];
 }
 
 function categorizePoints(grid, categories) {
@@ -112,72 +112,123 @@ function categorizePoints(grid, categories) {
         }
       }
 
-      let averageCategoryDistances = []
+      let averageCategoryDistances = [];
       for (name of terrainNames) {
         let categoryPoints = closestPoints.filter((point) => point.category.name == name);
 
-        let averageDistance = Infinity
+        let averageDistance = Infinity;
         if (categoryPoints.length != 0) {
           averageDistance = categoryPoints.reduce((sum, point) => sum + point.distance * point.distance, 0) / categoryPoints.length;
         }
         averageCategoryDistances.push([name, averageDistance]);
       }
-      averageCategoryDistances.sort((a, b) => a[1]-b[1])
+      averageCategoryDistances.sort((a, b) => a[1]-b[1]);
 
       grid[x][y].terrain = averageCategoryDistances[0][0];
       grid[x][y].calculatedDistances = averageCategoryDistances;
       grid[x][y].closestPoints = closestPoints;
     }
   }
-  return grid
+  return grid;
 }
-// categorizePoints(map, terrains)
-let gridScale = canvas.scene.grid.size;
-let sceneRect = canvas.scene.dimensions.sceneRect;
 
-//Initialize the grid to all void
-let map = new Array(sceneRect.width/gridScale);
-for (let i = 0; i < sceneRect.width/gridScale; i += 1) {
-  map[i] = new Array(sceneRect.height/gridScale);
-  for (let j = 0; j < sceneRect.height/gridScale; j += 1) {
-    map[i][j] = {
-      x: sceneRect.x+(i*gridScale), 
-      y: sceneRect.y+(j*gridScale), 
-      terrain: "void"
-    };
+function initializeGrid(sceneRect, gridScale, sceneRect) {
+  //Initialize the grid to all void
+  let map = new Array(sceneRect.width/gridScale);
+  for (let i = 0; i < sceneRect.width/gridScale; i += 1) {
+    map[i] = new Array(sceneRect.height/gridScale);
+    for (let j = 0; j < sceneRect.height/gridScale; j += 1) {
+      map[i][j] = {
+        x: sceneRect.x+(i*gridScale), 
+        y: sceneRect.y+(j*gridScale), 
+        terrain: "void"
+      };
+    }
   }
+  return map;
 }
 
-let d = canvas.drawings.children[0]; //<- all drawings
-for (let i = 0; i < d.children.length; i += 1) {
-  let points = convertRelativePointsToGlobal([d.children[i].shape.data.x, d.children[i].shape.data.y], d.children[i].shape.data.shape.points);
-  let globalPoints = points.map(point => canvas.grid.getTopLeft(point[0], point[1] ));
-  let drawingTerrain = terrainFromColor(d.children[i].document.strokeColor)
-  // console.log(drawingTerrain.color + " from " + d.children[i].document.strokeColor)
-  terrains[drawingTerrain].points.push(...globalPoints)
-}
-
-categorizePoints(map, terrains)
-
-let newTiles = [];
-for (let x = 0; x < map.length; x++) {
-  for (let y = 0; y < map[x].length; y++) {
-    let newTile = {};
-    newTile.x = map[x][y].x;
-    newTile.y = map[x][y].y;
-    newTile.texture = {};
-    newTile.texture.src = terrains[map[x][y].terrain].texture;
-    // newTile.texture.tint = "#690069";
-    newTile.width = gridScale;
-    newTile.height = gridScale;
-    newTiles.push(newTile);
-
+// Categorize drawings by color and add their points to the corresponding terrains
+function processDrawings(terrains) {
+  let d = canvas.drawings.children[0]; //<- all drawings
+  for (let i = 0; i < d.children.length; i += 1) {
+    let points = convertRelativePointsToGlobal([d.children[i].shape.data.x, d.children[i].shape.data.y], d.children[i].shape.data.shape.points);
+    let globalPoints = points.map(point => canvas.grid.getTopLeft(point[0], point[1] ));
+    let drawingTerrain = terrainFromColor(d.children[i].document.strokeColor);
+    // console.log(drawingTerrain.color + " from " + d.children[i].document.strokeColor)
+    terrains[drawingTerrain].points.push(...globalPoints);
   }
 }
 // TODO: Add some intermediate points if the bezier points are too far apart
-canvas.scene.createEmbeddedDocuments("Tile",newTiles);
+
+// create tiles based on the map
+function createTiles(map, terrains, gridScale) {
+  let newTiles = [];
+  for (let x = 0; x < map.length; x++) {
+    for (let y = 0; y < map[x].length; y++) {
+      let newTile = {};
+      newTile.x = map[x][y].x;
+      newTile.y = map[x][y].y;
+      newTile.texture = {};
+      newTile.texture.src = terrains[map[x][y].terrain].texture;
+      // newTile.texture.tint = "#690069";
+      newTile.width = gridScale;
+      newTile.height = gridScale;
+      newTiles.push(newTile);
+    }
+  }
+  return newTiles;
+}
 
 
+
+// add the appropriate textures to the grid based on corners.
+function autotileEdges() {
+
+}
+
+const moduleDirectory = "modules/MapMaker/";
+const terrains = {
+  water: {
+    color: "#0000FF",
+    name: "water",
+    texture: moduleDirectory+"tiles/"+"Water_tile.png",
+    points: [],
+    
+
+  },
+  grass: {
+    color: "#00FF00",
+    name: "grass",
+    texture: moduleDirectory+"tiles/"+"Grass_tile.png",
+    points: []
+  },
+  path: {
+    color: "#A0522D",
+    name: "path",
+    texture: moduleDirectory+"tiles/"+"Road_tile.png",
+    points: []
+  },
+  void: {
+    color: "#000000",
+    name: "void",
+    texture: "",
+    points: []
+  }
+};
+
+function main() {
+  let gridScale = canvas.scene.grid.size;
+  let sceneRect = canvas.scene.dimensions.sceneRect;
+
+  //Initialize the grid to all void
+  let map = initializeGrid(sceneRect, gridScale, sceneRect);
+  processDrawings(terrains);
+  categorizePoints(map, terrains);
+  newTiles = createTiles(map, terrains, gridScale);
+  return canvas.scene.createEmbeddedDocuments("Tile",newTiles);
+}
+main();
 
 
 //TODO: Apparently also create an entire color-picking module for the drawing tool? Or recommend the boneyard one.
